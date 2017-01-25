c0df0f555ff57975c835e326cbb5853a876660d6
Heitor Silva <heitor.silva@corp.globo.com>
2017-01-25 11:49:00 -0200

    Switching ack-grep to silversearcher-ag

--- install-basics.sh
+++ install-basics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-sudo apt-get install debconf-utils build-essential module-assistant vim locate ack-grep htop zip git -y
+sudo apt-get install debconf-utils build-essential module-assistant vim locate silversearcher-ag htop zip git -y
 sudo bash -c 'echo "syntax on;" >> ~/.vimrc'
 sudo bash -c 'echo "syntax on;" >> /root/.vimrc'
